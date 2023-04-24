9f9601e653618cc14d95b1b3c876672aa0756fd8
Johnny Proano <jproano22@yahoo.com>
2023-04-24 18:03:39 -0400

    styled layout of app and hardcoded main img

--- Dog.js
+++ Dog.js
@@ -1,1 +1,21 @@
 // Create the Dog class here
+
+class Dog {
+  constructor(data) {
+    Object.assign(this, data);
+  }
+
+  getDogHtml() {
+    const { name, avatar, age, bio } = this;
+    return `
+    <div class="dog-card">
+      <img class="dog-images" src="images/dog-teddy.jpg">
+      <h2 class="title">Teddy, 30</h2>
+      <p class="bio">How you doing?</p>
+    
+    </div>
+    `;
+  }
+}
+
+export default Dog;
--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // Remember to import the data and Dog class!
 import dogs from "./data.js";
+import Dog from "./Dog.js";
+
+// have to render a new dog profile on start
+// dog profile has to grab the html needed from the Dog Characteristcs for the Dog class
+
+let dogProfile = new Dog();
+
+function render() {
+  document.getElementById("image-dog-container").innerHTML = dogProfile.getDogHtml();
+}
+render();
