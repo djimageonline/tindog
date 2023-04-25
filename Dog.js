b3df20e5ef857057c8defc0db1fb1f1c1bc86319
Johnny Proano <jproano22@yahoo.com>
2023-04-24 21:19:46 -0400

    added function to heartbtn click and crossbtnclick.  Added getNewDogProfile

--- Dog.js
+++ Dog.js
@@ -6,14 +6,26 @@ class Dog {
   }
 
   getDogHtml() {
-    const { name, avatar, age, bio } = this;
+    const { name, avatar, age, bio, hasBeenSwiped, hasBeenLiked } = this;
     return `
-    <div class="dog-card">
-      <img class="dog-images" src="images/dog-teddy.jpg">
-      <h2 class="title">Teddy, 30</h2>
-      <p class="bio">How you doing?</p>
-    
-    </div>
+      <div class="dog-card">
+        <img class="dog-images" src=${avatar}>
+        <h2 class="title">${name}, ${age}</h2>
+        <p class="bio">${bio}</p>
+        ${hasBeenLiked ? this.setDogLiked() : hasBeenSwiped ? this.setDogDisliked() : ""}
+      </div>
+    `;
+  }
+
+  setDogLiked() {
+    return `
+    <div class="badge-like liked"><img src="images/badge-like.png"></img>
+    `;
+  }
+
+  setDogDisliked() {
+    return `
+    <div class="badge-disliked dis-liked"><img src="images/badge-nope.png"></img>
     `;
   }
 }
--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-// Remember to import the data and Dog class!
 import dogs from "./data.js";
 import Dog from "./Dog.js";
 
-// have to render a new dog profile on start
-// dog profile has to grab the html needed from the Dog Characteristcs for the Dog class
+const likedBtn = document.getElementById("heart-btn");
+const dislikedBtn = document.getElementById("cross-btn");
+likedBtn.addEventListener("click", heartBtnClick);
+dislikedBtn.addEventListener("click", crossBtnClick);
+let indexOfDog = 0;
 
-let dogProfile = new Dog();
+let dogProfileArray = new Dog(dogs[indexOfDog]);
+let isWaiting = false;
+
+function heartBtnClick() {
+  dogProfileArray.hasBeenLiked = true;
+  dogProfileArray.hasBeenSwiped = false;
+  likedBtn.classList.add("liked");
+  dislikedBtn.classList.remove("dis-liked");
+  render();
+  getNewDogProfile();
+}
+
+function crossBtnClick() {
+  dogProfileArray.hasBeenLiked = false;
+  dogProfileArray.hasBeenSwiped = true;
+  dislikedBtn.classList.add("dis-liked");
+  likedBtn.classList.remove("liked");
+  render();
+  getNewDogProfile();
+}
+
+function getNewDogProfile() {
+  setTimeout(function () {
+    console.log(indexOfDog);
+    if (indexOfDog < dogs.length) {
+      indexOfDog += 1;
+    }
+    console.log(indexOfDog);
+    dogProfileArray = new Dog(dogs[indexOfDog]);
+    render();
+  }, 3000);
+}
 
 function render() {
-  document.getElementById("image-dog-container").innerHTML = dogProfile.getDogHtml();
+  document.getElementById("image-dog-container").innerHTML = dogProfileArray.getDogHtml();
 }
 render();
